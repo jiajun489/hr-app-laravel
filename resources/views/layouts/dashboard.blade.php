625d003a38a4f9aa0234072d033fa65afcc2de44
RaidanRR <edward5raidan5497@gmail.com>
2025-05-28 03:47:04 +0700

    index.html bug

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -61,7 +61,7 @@
     <div class="sidebar-header position-relative">
         <div class="d-flex justify-content-between align-items-center">
             <div class="logo">
-                <a href="index.html"><img src="{{ asset('images/reltroner-hrm1.png') }}" alt="Logo" srcset=""></a>
+                <a href="{{ route('dashboard') }}"><img src="{{ asset('images/reltroner-hrm1.png') }}" alt="Logo" srcset=""></a>
             </div>
             <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
