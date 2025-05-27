9ddbe3c8e94be44cbb3408415496010995b22eff
RaidanRR <edward5raidan5497@gmail.com>
2025-05-27 12:10:18 +0700

    https://www.reltroner.com/blog/for-recruiters

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -814,7 +814,7 @@
                 </div>
                 <div class="float-end">
                     <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                        by <a href="https://saugi.me">Rei Reltroner</a></p>
+                        by <a href="https://www.reltroner.com/blog/for-recruiters">Rei Reltroner</a></p>
                 </div>
             </div>
         </footer>
